49dee340539cbcee4076708d93655282c60b23de
Madhangi09 <kamatchibala6@gmail.com>
2025-02-04 11:37:07 +0530

    Add files via upload

---
+++ cw_04_02_25.py
@@ -0,0 +1,37 @@
+#coversion of character
+character=input()
+if 'A'<=character<='Z':
+    c=ord(character)+32
+    converted=chr(c)
+print(converted)
+
+"""#lowercase conversion
+def upcase(ch):
+    for i in ch:
+        if 'A'<=i<='Z':
+            p=ord(i)
+            q=chr(p+32)
+        print(q,end='')
+ch=input()
+upcase(ch)
+
+#upercase and lowercase conversion
+def lowcase(ch):
+    p=ord(ch)
+    q=chr(p+32)
+    print(q,end='')
+def upcase(char):
+    x=ord(char)
+    y=chr(x-32)
+    print(y,end='')
+string1=input()
+for i in string1:
+    if 'A'<=i<='Z':
+        lowercase(i)
+    elif if 'a'<=i<='z':
+        uppercase(i)"""
+        
+    
+
+        
+    
